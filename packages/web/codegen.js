0c6f39b6855008e0fc22407ce6c9f7036220b267
show <show75@ezweb.ne.jp>
2020-09-04 14:59:56 +0900

    setup client with graphql

---
+++ packages/web/codegen.js
@@ -0,0 +1,31 @@
+module.exports = {
+  schema: [
+    {
+      [`${process.env.HASURA_GRAPHQL_ENDPOINT}`]: {
+        // headers: {
+        //   'x-hasura-admin-secret': process.env.HASURA_GRAPHQL_ADMIN_SECRET,
+        // },
+      },
+    },
+  ],
+  documents: ['./src/graphql/queries/*.graphql', './src/graphql/mutations/*.graphql'],
+  overwrite: true,
+  generates: {
+    './src/generated/graphql.tsx': {
+      plugins: [
+        'typescript',
+        'typescript-operations',
+        'typescript-react-apollo',
+      ],
+      config: {
+        skipTypename: false,
+        withHooks: true,
+        withHOC: false,
+        withComponent: false,
+      },
+    },
+    './graphql.schema.json': {
+      plugins: ['introspection'],
+    },
+  },
+};
--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -3,10 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { ApolloProvider ,ApolloClient, InMemoryCache, gql } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache()
+});
+
+client
+  .query({
+    query: gql`
+      query GetRates {
+        rates(currency: "USD") {
+          currency
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
